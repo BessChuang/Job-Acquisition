65b5fb0e17e5854df222df14aa24a43201529581
Bess <peiruchuang@gmail.com>
2024-04-15 22:38:05 +0800

    Add function of navbtn

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-const navbtn = document.getElementById("navbtn");
-navbtn.addEventListener(click, function toggle() { });
+
+const navbtn = document.getElementById("navbtn"),
+    navbtnbar = document.querySelector("header div div"),
+    navm1 = document.getElementById("navm1"),
+    navm2 = document.getElementById("navm2"),//m implies Mobile
+    navMain = document.getElementById("navMain"),
+    navExhibit = document.getElementById("navExhibit")
+
+navbtn.addEventListener("click", function () {
+    // if (navbtn.classList.contains("active")) {
+    navbtn.classList.toggle("active");
+    navbtnbar.classList.toggle("active");
+    navm1.classList.toggle("active");
+    navm2.classList.toggle("active");
+    navMain.classList.toggle("active");
+    navExhibit.classList.toggle("active");
+    console.log(1);
+});
